8bf1fa8a2edb0401a4a999f55404ee69e2f9761d
jiwoo <usekang24@gmail.com>
2022-09-23 17:18:23 +0900

    220923

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ navlist.on('mouseover focus', function(){
 navlist.on('mouseout blur', function(){
     $(this).find('ul').stop().slideUp();
 });
+
+let menu=$('.tab>li');
+let menulist=$('.tab-lists>div');
+
+const tab=$('.tab li');
+    const tabContent=$('.tabcontents>div');
+
+menu.click(function(){
+    let tg=$(this);
+    let i=tg.index();
+    menu.removeClass('active');
+    tg.addClass('active');
+
+    menulist.css('display','none');
+    menulist.eq(i).css('display','block');
+
+    return false
+});
